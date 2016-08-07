924087245c8135c16121e5543a94f0fb4b496399
Michael Laythe <mrlaythe24@aol.com>
2016-08-07 16:28:57 -0700

    abstracted out REST route logic into controllers for a more modular approach

--- server/user-routes.js
+++ server/user-routes.js
@@ -1,130 +1,22 @@
 const express = require('express'),
       _       = require('lodash'),
       config  = require('./config'),
-      jwt     = require('jsonwebtoken');
+      jwt     = require('jsonwebtoken'),
+      userController = require('./users/userController'),
+      tokenController = require('./util/tokenController');
 
 const app = module.exports = express.Router();
 
-//database of users goes here
-const users = [{
-  id: 1,
-  username: 'Admin',
-  password: '123'
-}];
+app.post('/family/create', userController.createFamily);
 
-const createAdminToken = (user, key) => {
-  user = _.omit(user, 'password');
-  user.admin = true;
-  user.key = key;
-  return jwt.sign(user, config.secret, { expiresIn: 60 * 60 * 5 });
-}
-
-const createToken = (user, key) => {
-  user = _.omit(user, 'password');
-  user.admin = false;
-  user.key = key;
-  return jwt.sign(user, config.secret, { expiresIn: 60 * 60 * 5 });
-}
-
-const createFamilyKey = user => {
-  let id = (Math.random().toString(36)+'00000000000000000').slice(2, 5 + 2);
-
-  return user.familyName + id;
-}
-
-const getUserScheme = req => {
-  let username,
-      type,
-      userSearch = {};
-
-  // POST contains a username and not an email
-  if (req.body.username) {
-    username = req.body.username;
-    type = 'username';
-    userSearch = { username: username };
-  }
-  // POST contains an email and not an username
-  else if (req.body.email) {
-    username = req.body.email;
-    type = 'email';
-    userSearch = { email: username };
-  }
-
-  return {
-    username: username,
-    type: type,
-    userSearch: userSearch
-  }
-}
-
-app.post('/family/create', (req, res) => {
-
-  const userScheme = getUserScheme(req);
-
-  if (!userScheme.username || !req.body.password || !req.body.familyName) {
-    return res.status(400).send("Missing username or password or family name.");
-  }
-
-  if (_.find(users, userScheme.userSearch)) {
-   return res.status(400).send("That username is taken.");
-  }
-
-  let profile = _.pick(req.body, userScheme.type, 'password', 'familyName', 'extra');
-  profile.id = _.max(users, 'id').id + 1;
-
-  users.push(profile);
-
-  let familyKey = createFamilyKey(profile);
-
-  res.status(201).send({
-    id_token: createAdminToken(profile, familyKey),
-    family_key: familyKey
-  });
-});
-
-app.post('/family/join', (req, res) => {
-  
-  const userScheme = getUserScheme(req);
-
-  if (!userScheme.username || !req.body.password || !req.body.familyKey) {
-    return res.status(400).send("Missing username or password or family key.");
-  }
-
-  if (_.find(users, userScheme.userSearch)) {
-   return res.status(400).send("That username is taken.");
-  }
-
-  let profile = _.pick(req.body, userScheme.type, 'password', 'familyKey', 'extra');
-  profile.id = _.max(users, 'id').id + 1;
-
-  users.push(profile);
-
-  let familyKey = createFamilyKey(profile);
-
-  res.status(201).send({
-    id_token: createToken(profile, familyKey)
-  });
-});
+app.post('/family/join', userController.joinFamily);
 
 app.post('/sessions/create', function(req, res) {
-
-  let userScheme = getUserScheme(req);
-
-  if (!userScheme.username || !req.body.password) {
+  if (!req.body.username || !req.body.password) {
     return res.status(400).send("You must send the username and the password");
   }
 
-  let user = _.find(users, userScheme.userSearch);
-
-  if (!user) {
-    return res.status(401).send("The username or password don't match");
-  }
-
-  if (user.password !== req.body.password) {
-    return res.status(401).send("The username or password don't match");
-  }
-
   res.status(201).send({
-    id_token: createToken(user)
+    id_token: tokenController.createToken(user)
   });
 });
