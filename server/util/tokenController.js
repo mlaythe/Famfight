b8c0acce0e10b1a55389a104b3f8cd5b6dc85ce3
Michael Laythe <mrlaythe24@aol.com>
2016-08-07 19:31:37 -0700

    extracted methods that deal with creating jwts when families are created or joined

---
+++ server/util/tokenController.js
@@ -0,0 +1,27 @@
+const _ = require('lodash'),
+      jwt = require('jsonwebtoken');
+      config = require('../config');
+      
+const tokenController = {};
+
+tokenController.createAdminToken = (user, key) => {
+  user = _.omit(user, 'password');
+  user.admin = true;
+  user.key = key;
+  return jwt.sign(user, config.secret, { expiresIn: 60 * 60 * 5 });
+};
+
+tokenController.createToken = (user, key) => {
+  user = _.omit(user, 'password');
+  user.admin = false;
+  user.key = key;
+  return jwt.sign(user, config.secret, { expiresIn: 60 * 60 * 5 });
+};
+
+tokenController.createFamilyKey = user => {
+  let id = (Math.random().toString(36) + '00000000000000000').slice(2, 5 + 2);
+
+  return user.familyName + id;
+};
+
+module.exports = tokenController;
