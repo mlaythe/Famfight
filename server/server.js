7a1cf796d742d5a86eedf5dc1138f74eacd2ad02
Michael Laythe <mrlaythe24@aol.com>
2016-10-23 16:22:01 -0700

    using cors

--- server/server.js
+++ server/server.js
@@ -5,21 +5,23 @@ const bodyParser = require('body-parser');
 const webpackConfig = require('../webpack.config');
 const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 const DEVPORT = 9090;
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, './../')));
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'test') {
   app.use(logger('dev'));
 }
 
-app.use(require('./protected-routes'));
 app.use(require('./user-routes'));
+app.use(require('./protected-routes'));
 
 app.listen(PORT, err => console.log('listening on http://localhost:' + PORT));
 
