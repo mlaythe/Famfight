7f065ccc63adde270303dd0ea599e8da21e3cb4d
Michael Laythe <mrlaythe24@aol.com>
2016-11-13 10:29:27 -0800

    attach react-router-redux for history tracking, and have react dom render react-router routes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import { Router, Route, browserHistory, Redirect } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
 import store from './store.js';
 
 import App from './components/App';
 
+const history = syncHistoryWithStore(browserHistory, store);
+
 render(
   <Provider store={store}>
-    <App />
+    <Router history={history}>
+      <Route path="/" component={App} />
+      <Redirect path="*" to="/" />
+    </Router>
   </Provider>,
   document.getElementById('app')
 );
