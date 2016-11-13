946f25f6419a5bebb977a4c5ebc16b210a534d29
Michael Laythe <mrlaythe24@aol.com>
2016-11-13 12:17:10 -0800

    switch to /home upon successful signup

--- src/actions/impureActions.js
+++ src/actions/impureActions.js
@@ -1,22 +1,22 @@
 import axios from 'axios';
 import { requestUserInfoAction, receiveUserInfoAction, failureUserInfoAction } from './fetchEventActions.js';
 import { emptySignupFieldAction } from './signupActions.js';
+import { browserHistory } from 'react-router';
 
 axios.defaults.adapter = require('axios/lib/adapters/http');
 
 export function createUserInfoAction(username, password) {
   return function (dispatch) {
-    // dispatch(requestUserInfoAction());
     return axios.post('http://localhost:8080/user/signup', { username, password })
       .then((response) => {
         if (!response.data.id_token) {
           throw new Error('Error signing up user.');
         }
 
-        dispatch(receiveUserInfoAction(response.data.events));
+        browserHistory.push('/home');
       })
       .catch((error) => {
-        console.log('inside catch');
+        console.log('inside catch', error);
         if (error.response) {
           dispatch(failureUserInfoAction());
         } else {
