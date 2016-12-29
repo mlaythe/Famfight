8b76813fc112ea03988fc28df2fa3cc48f19f769
Michael Laythe <mrlaythe24@aol.com>
2016-12-28 21:58:01 -0800

    add methods to handle logic of logging in and send post to .users/login for validation

--- src/actions/impureActions.js
+++ src/actions/impureActions.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { requestUserInfoAction, receiveUserInfoAction, failureUserInfoAction } from './fetchEventActions.js';
 import { signupErrorAction } from './signupActions.js';
+import { loginErrorAction } from './loginActions.js';
 import { browserHistory } from 'react-router';
 
 axios.defaults.adapter = require('axios/lib/adapters/http');
@@ -11,6 +12,29 @@ export function createUserInfoAction(username, password) {
       .then((response) => {
         if (!response.data.id_token) {
           throw new Error('Error signing up user.');
+        }
+
+        localStorage.setItem('token', response.data.id_token);
+        browserHistory.push('/home');
+      })
+      .catch((error) => {
+        console.log('inside catch', error);
+        if (error.response) {
+          dispatch(failureUserInfoAction());
+        } else {
+          console.log('inside error');
+          throw new Error('Error preparing ajax request');
+        }
+      });
+  };
+}
+
+export function loginUserAction(username, password) {
+  return function (dispatch) {
+    return axios.post('http://localhost:8080/user/login', { username, password })
+      .then((response) => {
+        if (!response.data.id_token) {
+          throw new Error('Error logging in user.');
         }
 
         localStorage.setItem('token', response.data.id_token);
@@ -47,6 +71,31 @@ export function submitSignupAction(username, password) {
       }));
     } else {
       dispatch(createUserInfoAction(username, password));
+    }
+
+    return;
+  };
+}
+
+export function submitLoginAction(username, password) {
+  return function (dispatch) {
+    if (!username && !password) {
+      dispatch(loginErrorAction({
+        errorMsgID: 'emptyUsernamePasswordFieldError',
+        errorMsgText: 'Missing username and password',
+      }));
+    } else if (!username) {
+      dispatch(loginErrorAction({
+        errorMsgID: 'emptyUsernameFieldError',
+        errorMsgText: 'Missing username',
+      }));
+    } else if (!password) {
+      dispatch(loginErrorAction({
+        errorMsgID: 'emptyPasswordFieldError',
+        errorMsgText: 'Missing password',
+      }));
+    } else {
+      dispatch(loginUserAction(username, password));
     }
 
     return;
