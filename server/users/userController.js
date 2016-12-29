3be3f9b935ee0ff6ca3ac61ad85ed2241f071e79
Michael Laythe <mrlaythe24@aol.com>
2016-12-28 20:47:29 -0800

    don't pass req.body.username to createToken anymore

--- server/users/userController.js
+++ server/users/userController.js
@@ -35,25 +35,21 @@ userController.authenticateUser = (req, res, next) => {
 };
 
 userController.createUser = (req, res, next) => {
-  // if (!req.body.name || !req.body.email || !req.body.password) {
-  //   return res.status(401).send('Missing name, email, or password!');
-  // }
-
   userController.createTable()
     .then(() => {
       User
-        .query({where: { username: req.body.username }})
+        .query({ where: { username: req.body.username }})
         .fetch()
         .then(model => {
           if (model) {
             return res.status(400).send('Username is already taken.');
           }
-        
+          
           userController.encryptPassword(req.body);
 
           User.forge(req.body).save().then(result => {
             return res.status(201).send({
-              id_token: tokenController.createToken(result.attributes, req.body.username)
+              id_token: tokenController.createToken(result.attributes),
             });
           });
         });
