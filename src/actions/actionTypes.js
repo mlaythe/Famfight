3e9c8938bb6249bd8f7338b87d2075b9043ad4ba
Michael Laythe <mrlaythe24@aol.com>
2016-12-28 21:50:54 -0800

    change signup_error to error temporarily to generalize errors for both login and signup

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,7 +1,8 @@
-//Signup
+// TODO change ERROR action to something more specific
+// Signup && Login
 export const USERNAME_INPUT = 'USERNAME_INPUT';
 export const PASSWORD_INPUT = 'PASSWORD_INPUT';
-export const SIGNUP_ERROR = 'SIGNUP_ERROR';
+export const ERROR = 'ERROR';
 export const FETCH_USER_INFO_REQUEST = 'FETCH_USER_INFO_REQUEST';
 export const FETCH_USER_INFO_SUCCESS = 'FETCH_USER_INFO_SUCCESS';
 export const FETCH_USER_INFO_FAILURE = 'FETCH_USER_INFO_FAILURE';
