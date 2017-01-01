9216425c528139b029d37c4a9e6defb228045dc9
Michael Laythe <mrlaythe24@aol.com>
2017-01-01 01:01:03 -0800

    use sequelize API

--- server/users/userController.js
+++ server/users/userController.js
@@ -7,63 +7,46 @@ const bcrypt = require('bcrypt');
 const SALT_FACTOR = 10;
 const userController = {};
 
-userController.createTable = () => {
-  return knex.schema.createTableIfNotExists('users', (user) => {
-    user.increments();
-    user.string('username');
-    user.string('password');
-  });
-};
-
 userController.authenticateUser = (req, res, next) => {
-  User
-    .query({ where: { email: req.body.email } })
-    .fetch()
-    .then((model) => {
-      if (!model) {
-        return res.status(400).send('Invalid email');
-      }
+  User.findOne({
+    where: {
+      username: req.body.username,
+    },
+  })
+  .then((user) => {
+    if (!user) {
+      return res.status(400).send('Incorrect credentials.');
+    }
 
-      const isValidUser = userController.decryptPassword(req.body, model.attributes.password);
-      
-      return isValidUser ? next() : res.status(401).send('Password does not match our records.');
-    })
-    .catch((err) => {
-      return res.status(400).send('Error authenticating user.');
-    });
+    const isValidUser = userController.decryptPassword(req.body, user.dataValues.password);
+
+    return isValidUser ? next() : res.status(401).send('Incorrect credentials.');
+  })
+  .catch((err) => console.error(err));
 };
 
 userController.createUser = (req, res, next) => {
-  userController.createTable()
-    .then(() => {
-      User
-        .query({ where: { username: req.body.username } })
-        .fetch()
-        .then((model) => {
-          if (model) {
-            return res.status(400).send('Username is already taken.');
-          }
-          
-          userController.encryptPassword(req.body);
+  User.findOne({
+    where: {
+      username: req.body.username,
+    },
+  })
+  .then((user) => {
+    if (user) {
+      return res.status(400).send('Username is already taken.');
+    }
 
-          User.forge(req.body).save().then((result) => {
-            return res.status(201).send({
-              id_token: authUtils.createToken(result.attributes),
-            });
-          });
-        });
+    userController.encryptPassword(req.body);
+
+    User.create(req.body).then((user) => {
+      return res.status(201).send({
+        id_token: authUtils.createToken(user.dataValues),
+      });
     })
-    .catch((err) => {
-      return res.status(400).send('Error adding user to database: ', err.message);
-    });
+    .catch((err) => console.error(err));
+  })
+  .catch((err) => console.error(err));
 };
-
-// userController.createUsernameID = (user) => {
-//   const ID = (Math.random().toString(36) + '00000000000000000').slice(2, 5 + 2);
-//   const indexOfAt = user.email.indexOf('@');
-
-//   user.emailID = user.email.slice(0, indexOfAt) + ID;
-// };
 
 userController.encryptPassword = (user) => {
   const salt = bcrypt.genSaltSync(SALT_FACTOR);
