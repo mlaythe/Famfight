d2199e19d303690e1af7f65ca55e160023472442
Michael Laythe <mrlaythe24@aol.com>
2017-01-01 12:44:03 -0800

    add tests for POST /signup & /login

---
+++ test/server/routes/userRoutes.js
@@ -0,0 +1,117 @@
+const { expect } = require('chai');
+const app = require('../../../server/server');
+const request = require('supertest-as-promised');
+const faker = require('faker');
+const User = require('../../../server/users/userModel');
+
+describe('POST /users/signup', () => {
+  beforeEach((done) => {
+    User.sync({ force: true }).then(() => done());
+  });
+
+  it('should signup user with proper request body', (done) => {
+    request(app)
+      .post('/users/signup')
+      .send({
+        username: faker.internet.userName(),
+        password: faker.internet.password(),
+      })
+      .expect(201)
+      .end((err, res) => {
+        if (err) throw err;
+        expect(res.body.id_token).to.exist;
+        done();
+      });
+  });
+
+  it('should not allow signup when missing username', (done) => {
+    request(app)
+      .post('/users/signup')
+      .send({
+        password: faker.internet.password(),
+      })
+      .expect(400)
+      .end((err, res) => {
+        if (err) throw err;
+        expect(res.body.id_token).to.not.exist;
+        done();
+      });
+  });
+
+  it('should not allow signup when missing password', (done) => {
+    request(app)
+      .post('/users/signup')
+      .send({
+        username: faker.internet.userName(),
+      })
+      .expect(400)
+      .end((err, res) => {
+        if (err) throw err;
+        expect(res.body.id_token).to.not.exist;
+        done();
+      });
+  });
+
+  it('should not allow signup when username is shorter than 4 characters', (done) => {
+    request(app)
+      .post('/users/signup')
+      .send({
+        username: 'hi',
+        password: faker.internet.password(),
+      })
+      .expect(400)
+      .end((err, res) => {
+        if (err) throw err;
+        expect(res.body.id_token).to.not.exist;
+        done();
+      });
+  });
+
+  it('should not allow signup when password is shorter than 4 characters', (done) => {
+    request(app)
+      .post('/users/signup')
+      .send({
+        username: faker.internet.userName(),
+        password: 'we2',
+      })
+      .expect(400)
+      .end((err, res) => {
+        if (err) throw err;
+        expect(res.body.id_token).to.not.exist;
+        done();
+      });
+  });
+});
+
+describe('POST /users/login', () => {
+  beforeEach((done) => {
+    User.sync({ force: true }).then(() => {
+      request(app)
+        .post('/users/signup')
+        .send({
+          username: 'mlaythe',
+          password: '1234',
+        })
+        .expect(201)
+        .end((err, res) => {
+          if (err) throw err;
+          done();
+        })
+    });
+  });
+
+  it('should login user with proper request body', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        username: 'mlaythe',
+        password: '1234',
+      })
+      .expect(200)
+      .end((err, res) => {
+        if (err) throw err;
+        expect(res.body.id_token).to.exist;
+        done();
+      });
+  });
+});
