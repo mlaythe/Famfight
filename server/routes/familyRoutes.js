5d666a39f02dd25f980b0f7ed331ab4432f14882
Michael Laythe <mrlaythe24@aol.com>
2016-12-31 23:31:37 -0800

    keep routes linked with controllers

---
+++ server/routes/familyRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const familyController = require('../families/familyController');
+
+const app = module.exports = express.Router();
+
+app.post('/families/create', familyController.createFamily);
+
+app.post('/families/join', familyController.joinFamily);
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,12 +1,6 @@
 const express = require('express');
-const userController = require('./users/userController.js');
-const familyController = require('./families/familyController');
-const tokenController = require('./util/tokenController');
+const userController = require('../users/userController.js');
 
 const app = module.exports = express.Router();
 
 app.post('/users/signup', userController.createUser);
-
-app.post('/families/create', familyController.createFamily);
-
-app.post('/families/join', familyController.joinFamily);
