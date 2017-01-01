fdad6d03d31a8c1f17533281106724b3d297ab8f
Michael Laythe <mrlaythe24@aol.com>
2017-01-01 01:00:44 -0800

    get rid of excess whitespace

--- server/routes/familyRoutes.js
+++ server/routes/familyRoutes.js
@@ -4,5 +4,4 @@ const familyController = require('../families/familyController');
 const app = module.exports = express.Router();
 
 app.post('/families/create', familyController.createFamily);
-
 app.post('/families/join', familyController.joinFamily);
