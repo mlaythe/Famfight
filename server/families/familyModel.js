9c03cc731015ad82a41a13a9d716a4b2334b3252
Michael Laythe <mrlaythe24@aol.com>
2017-01-01 01:00:36 -0800

    add creator and name fields, and creator is foreign key to users table

--- server/families/familyModel.js
+++ server/families/familyModel.js
@@ -1,8 +1,14 @@
-'use strict';
-const { bookshelf, knex } = require('./../database');
+const Sequelize = require('sequelize');
+const sequelize = require('../database');
+const User = require('../users/userModel');
 
-const Family = bookshelf.Model.extend({
-  tableName: 'families'
+const Family = sequelize.define('family', {
+  creator: Sequelize.STRING,
+  name: Sequelize.STRING,
 });
+
+Family.belongsTo(User);
+
+Family.sync();
 
 module.exports = Family;
