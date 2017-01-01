f677ead660e0d9fd367f4dff93f07d82999ec301
Michael Laythe <mrlaythe24@aol.com>
2016-12-31 23:41:45 -0800

    add POST /login

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,3 +4,4 @@ const userController = require('../users/userController.js');
 const app = module.exports = express.Router();
 
 app.post('/users/signup', userController.createUser);
+app.post('/users/login', userController.authenticateUser);
